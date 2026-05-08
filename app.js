03573c5810e469f651d2db79314d8e6e5d67c662
MagnusFlod <magnus.flodstrom@hotmail.com>
2026-05-08 11:40:42 +0200

    Some small changes

--- app.js
+++ app.js
@@ -6,8 +6,6 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
 var participantRouter = require('./routes/participant');
 
 var app = express();
@@ -26,8 +24,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+
 app.use('/participant', participantRouter);
 
 // catch 404 and forward to error handler
